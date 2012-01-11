d5d7d5f25d70b7cb3c71cdd315483ffe9bd01834
Nick Malcolm <nick@southgatelabs.com>
2012-01-11 17:23:50 +1300

    Adds home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Usnapus::Application.routes.draw do
   resources :signups
+  
+  root :to => "home#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
