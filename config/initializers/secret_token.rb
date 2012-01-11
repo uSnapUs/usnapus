52154eeee03cdafbdcb27cb1d5fb08acbb862a86
Nick Malcolm <nick@southgatelabs.com>
2012-01-11 17:06:13 +1300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Usnapus::Application.config.secret_token = '4262484191d1f3021bdd921b9dca66a7c133dfa0a2949d6a8d5ef42d34e3759391602aff2df8205f8e4987c6c27c57b5010eb603b70cc98ec5234d920ab1d85d'
