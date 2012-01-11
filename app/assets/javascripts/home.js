febfccbcf5fa509b8c2c571f6bf2489334ca7c80
Nick Malcolm <nick@southgatelabs.com>
2012-01-11 19:48:38 +1300

    Javascriptify

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -4,5 +4,11 @@ $(document).ready(function() {
   
   $(".cta").on("click", function(){
     $('html, body, .content').animate({scrollTop: $("#the_black").position().top}, "slow");
+    return false;
   });
+  
+  $("#new_signup").on("ajax:beforeSend", function(){
+    $(".dropus").hide().html("Thanks, we love you! We'll be in touch soon :)<br />Have a fantastic day.").fadeIn()
+    return false;
+  })
 })
