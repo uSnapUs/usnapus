d72ea000ae020faf3235ba26cd3e4f33c8b17657
Nick Malcolm <nick@southgatelabs.com>
2012-01-11 19:53:39 +1300

    Fix db

--- db/schema.rb
+++ db/schema.rb
@@ -13,40 +13,11 @@
 
 ActiveRecord::Schema.define(:version => 20120111040726) do
 
-  create_table "events", :force => true do |t|
-    t.string   "name"
-    t.string   "location"
-    t.decimal  "latitude",   :precision => 10, :scale => 7
-    t.decimal  "longitude",  :precision => 10, :scale => 7
-    t.string   "shortcode"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "signups", :force => true do |t|
     t.datetime "event_date"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
