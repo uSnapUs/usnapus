a9a5cba358ebcc4c8c150b9bf1029afcd7ace7ef
Owen Evans <owen@bgeek.net>
2012-01-13 20:30:35 +1300

    validate presence of email and fix placeholder in older browsers

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
-  
+  $('#signup_email').placeholder();
   $("#the_black").css("height", $(window).height());
   
   $(".cta").on("click", function(){
     $('html, body, .content').animate({scrollTop: $("#the_black").position().top}, "slow");
     return false;
   });
-  
+  $('.new_signup').submit(function(){
+  	if($('input#signup_email').val().length<1)
+  	{
+  		return false;
+  	}
+  });
   $("#new_signup").on("ajax:beforeSend", function(){
   	$(".new_signup").hide();
     $(".dropus").hide().html("Thanks, we love you! We'll be in touch soon :)<br />Have a fantastic day.").fadeIn();
