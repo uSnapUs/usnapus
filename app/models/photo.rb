7562ca20cb3cade2723343fcfd05c2094f548dc9
Nick Malcolm <nick@southgatelabs.com>
2012-01-19 18:16:08 +1300

    Photo tests

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,3 @@
 class Photo < ActiveRecord::Base
   mount_uploader :photo, PhotoUploader
-  validates_presence_of :photo
 end
--- test/factories.rb
+++ test/factories.rb
@@ -1,2 +1,6 @@
 Factory.define :event do |e|
 end
+
+Factory.define :photo do |p|
+  p.photo "uploads/fake_image.jpg"
+end
