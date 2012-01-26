ef28bce5e39aa1589e83b39589d4f8771476a4ff
Nick Malcolm <nick@southgatelabs.com>
2012-01-26 21:54:49 +1300

    Require both lat and long for event finding

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
   # GET /events.json
   def index
     
-    if params[:latitude]
+    if params[:latitude] && params[:longitude]
       @events = Event.near origin: [params[:latitude].to_f, params[:longitude].to_f], within: 0.62
     elsif params[:code]
       @events = Event.find_by_code(params[:code]) || {}
