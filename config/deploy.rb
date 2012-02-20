97cd8d0798b673ba6eef47511b2fdf3080a1a0d8
uSnap.us Deploy <team@usnap.us>
2012-02-20 11:57:17 +0000

    Adds commit stuff

---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+set :user, 'deploy'
+set :domain, 'app1.usnap.us'
+set :application, "usnapus"
+ 
+set :repository, "git@github.com:uSnapUs/usnapus.git"  # Your clone URL
+set :scm, "git"
+set :branch, "production"
+set :scm_verbose, true
+set :deploy_via, :remote_cache
+set :scm_passphrase, "password"  # The deploy user's password
+set :deploy_to, lambda {"/home/#{user}/application"}
+set :use_sudo, false
+ 
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+ 
+role :web, domain                         # Your HTTP server, Apache/etc
+role :app, domain                         # This may be the same as your `Web` server
+role :db,  domain, :primary => true       # This is where Rails migrations will run
+ 
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
