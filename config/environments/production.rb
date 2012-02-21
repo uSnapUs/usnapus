d130caaaa11e23f9e7da827bcffacd69f17f9cb1
Nick Malcolm <nick@southgatelabs.com>
2012-02-22 02:09:14 +1300

    Maybe this will help

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,8 +44,6 @@ Usnapus::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   #config.assets.precompile
-  config.assets.compile = true
-  config.assets.initialize_on_precompile = false
   
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
