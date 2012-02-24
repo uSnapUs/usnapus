ef3dcd1d01bc455ac8181f6485efe48ebb223516
Nick Malcolm <nick@southgatelabs.com>
2012-02-25 00:37:49 +1300

    right gem

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
 require "rvm/capistrano"                               # Load RVM's capistrano plugin.
-set :rvm_ruby_string, 'ruby-1.9.2@usnapus'             # Or whatever env you want it to run in.
+set :rvm_ruby_string, 'ruby-1.9.2-p290@usnapus'        # Or whatever env you want it to run in.
 
 set :application, "usnapus"
 
