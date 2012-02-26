507c0a70aaeaac2b9a61140f4e31aab335c3247d
Owen Evans <owen@xero.com>
2012-02-27 08:50:34 +1300

    adding rvmc trust task

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,6 +43,15 @@ namespace :deploy do
   end
 end
 
+namespace :rvm do
+  desc 'Trust rvmrc file'
+  task :trust_rvmrc do
+    run "rvm rvmrc trust #{current_release}"
+  end
+end
+
+after "deploy:update_code", "rvm:trust_rvmrc"
+
 after "deploy:restart" do
 end
 load "deploy/assets"
