e2a205b1454f27b9d7306cf1205c9c6532bbf4f6
Owen Evans <owen@bgeek.net>
2012-02-29 13:52:39 +1300

    selector isn't null, but there's no element with the selector
    Signed-off-by: Owen Evans <owen@bgeek.net>

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -39,9 +39,9 @@ $(document).ready(function() {
 
   window.initializeMap = function(selector){
   //MAP
-  if(!selector)
+  if(!selector||!document.getElementById(selector))
     return;
-  
+
     var latlng = new google.maps.LatLng(37.793508,-122.419281); //SF
     var options = {
       zoom: 10,
