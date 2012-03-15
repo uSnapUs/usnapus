113518bfa542891356486a2e0a098fb1039c35dc
Nick Malcolm <nick@southgatelabs.com>
2012-03-15 22:54:03 +1300

    Users don't have to confirm password on sign up, devise changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   has_many :attendees
   has_many :events, through: :attendees
   has_many :photos, as: :creator
+  before_validation :skip_password_confirmation, on: :create
   
   attr_accessible :email, :password, :password_confirmation, :remember_me
   
@@ -17,5 +18,10 @@ class User < ActiveRecord::Base
   def name
     email
   end
+  
+  private
+    def skip_password_confirmation
+      self.password_confirmation = self.password
+    end
   
 end
