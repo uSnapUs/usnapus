401179f7d8d55a2155ad6363d526ea90632bf882
Nick Malcolm <nick@southgatelabs.com>
2012-03-25 14:00:52 +1300

    BUg fixes for URLs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     ["12am"] + (1..11).map{|i|"#{i}am"} + ["12pm"] + (1..11).map{|i|"#{i}pm"}
   end
   
+  def event_code_path(event, options = {})
+    "/#{event.code}#{options[:suffix]}"
+  end
+  
 end
