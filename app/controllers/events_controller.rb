3b306c95f61d597565d26927dd31a5d86ba75471
Nick Malcolm <nick@southgatelabs.com>
2012-04-05 13:08:01 +1200

    Add flash message for people who pay

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -62,6 +62,9 @@ class EventsController < ApplicationController
         at.user = current_user
         at.is_admin = true
       end
+      unless @event.free
+        flash[:notice] = "You're good to go! We'll invoice you soon"
+      end
       redirect_to event_photos_path @event
     else
       flash[:error] = "Please fix the errors below"
