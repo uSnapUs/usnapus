5b5944045f7b1329b954f10c0a4f90f9c9339c70
Nick Malcolm <nick@southgatelabs.com>
2012-04-11 20:19:15 +1200

    Add event_id index for photos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120401100058) do
+ActiveRecord::Schema.define(:version => 20120411081711) do
 
   create_table "attendees", :force => true do |t|
     t.integer  "user_id"
@@ -23,6 +23,29 @@ ActiveRecord::Schema.define(:version => 20120401100058) do
 
   add_index "attendees", ["event_id"], :name => "index_attendees_on_event_id"
   add_index "attendees", ["user_id"], :name => "index_attendees_on_user_id"
+
+  create_table "billing_details", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "card_type"
+    t.string   "card_name"
+    t.string   "last_four_digits"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+  end
+
+  add_index "billing_details", ["user_id"], :name => "index_billing_details_on_user_id"
+
+  create_table "charge_attempts", :force => true do |t|
+    t.integer  "billing_detail_id"
+    t.boolean  "success"
+    t.string   "message"
+    t.string   "authorization"
+    t.integer  "amount"
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
+  end
+
+  add_index "charge_attempts", ["billing_detail_id"], :name => "index_charge_attempts_on_billing_detail_id"
 
   create_table "devices", :force => true do |t|
     t.string   "guid"
@@ -73,6 +96,8 @@ ActiveRecord::Schema.define(:version => 20120401100058) do
     t.integer  "creator_id"
   end
 
+  add_index "photos", ["event_id"], :name => "index_photos_on_event_id"
+
   create_table "signups", :force => true do |t|
     t.datetime "event_date"
     t.string   "email"
@@ -95,8 +120,8 @@ ActiveRecord::Schema.define(:version => 20120401100058) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "authentication_token"
-    t.datetime "created_at",                                            :null => false
-    t.datetime "updated_at",                                            :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "name"
   end
 
