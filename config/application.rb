ffc160b10cf591a16a6810d7f8368c3504642640
Nick Malcolm <nick@southgatelabs.com>
2012-08-03 22:12:55 +1200

    Filter the proper attachment content

--- config/application.rb
+++ config/application.rb
@@ -39,8 +39,8 @@ module Usnapus
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password, :new_password, :number, :verification_value, :month, :year, "TextBody"]
-
+    config.filter_parameters += [:password, :new_password, :number, :verification_value, :month, :year, "Content\\z"]
+    
     # Enable the asset pipeline
     config.assets.enabled = true
 
