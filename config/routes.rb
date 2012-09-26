def502482250a30a33602ba9846e32fa266f32bf
Owen Evans <owen@xero.com>
2012-09-26 12:08:24 +1200

    fix redirect urls

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Usnapus::Application.routes.draw do
   
   devise_for :users, :controllers => { :registrations => 'registrations' }
   
-  root :to=> redirect("http://blog.usnap.us/")
+  root :to=> redirect("http://blog.usnap.us/post/123456/final")
   put "change_currency", to: "home#change_currency"
   get "geocode_search", to: "home#geocode_search"
   
@@ -19,12 +19,11 @@ Usnapus::Application.routes.draw do
     end
     resources :purchases, only: [:new, :create]
   end
-  
 
 
 
-  match "weddingshow"=> redirect("http://blog.usnap.us/")
-  match "welcome/:path"=> redirect("http://blog.usnap.us/")
+  match "weddingshow"=> redirect("http://blog.usnap.us/post/123456/final")
+  match "welcome/:path"=> redirect("http://blog.usnap.us/post/123456/final")
 
   resources :signups, only: [:create]
   
